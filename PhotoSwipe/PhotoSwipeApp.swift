c3bd871e9b20df31523380fa7aaebd9d0f461865
Tsing Chan <motoleisure@163.com>
2025-06-04 20:44:11 +0800

    Add PhotoSwipeApp.swift - Main app entry point

---
+++ PhotoSwipe/PhotoSwipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoSwipeApp.swift
+//  PhotoSwipe
+//
+//  Created by Developer on 2024/12/19.
+//
+
+import SwiftUI
+
+@main
+struct PhotoSwipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
